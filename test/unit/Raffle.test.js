d186ec9030ddb8bffb9167e671b2578e7172b0f5
Elee Lawleit <alibutt200235@gmail.com>
2023-08-10 12:11:03 +0500

    more tests

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -28,7 +28,7 @@ const { assert, expect } = require("chai");
         interval = await raffle.getInterval();
       });
 
-      describe("constructor", async function () {
+      describe("constructor", function () {
         it("Initializes the raffle correctly", async function () {
           const raffleState = await raffle.getRaffleState();
           //enum value 0 for open, 1 for closed/calculating
@@ -38,7 +38,7 @@ const { assert, expect } = require("chai");
         });
       });
 
-      describe("enterRaffle", async function () {
+      describe("enterRaffle", function () {
         it("reverts when you don't pay enough", async function () {
           await expect(raffle.enterRaffle()).to.be.revertedWith(
             "Raffle__NotEnoughETHEntered"
@@ -74,4 +74,47 @@ const { assert, expect } = require("chai");
             await expect(raffle.enterRaffle({value: raffleEntranceFee})).to.be.revertedWith("Raffle__NotOpen");
         })
       });
+      describe("checkUpkeep", function(){
+        it("returns false if people haven't sent any ETH", async function(){
+          await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
+          await network.provider.send("evm_mine", []);
+          //since checkUpkeep is a public function, but not view, calling it gonna start a transaction, but don't want that, we only want to see what it's gonna return, if we were to call it
+          //for this, we can use static calls, to see what it would return if called
+          //note that we didn't enter the raffle in this "it", so no players mean, no eth sent
+          const {upkeepNeeded} = await raffle.callStatic.checkUpkeep([]);
+          assert(!upkeepNeeded);
+        })
+
+        it("returns false if raffle isn't open", async function(){
+          await raffle.enterRaffle({value: raffleEntranceFee});
+          await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
+          await network.provider.send("evm_mine", []);
+          await raffle.performUpkeep([]) //[] is same as "0x empty bytes object"
+          const raffleState = await raffle.getRaffleState();
+          //should return false when state is closed
+          const {upkeepNeeded} = await raffle.callStatic.checkUpkeep([]);
+          //should be closed
+          assert.equal(raffleState.toString(), "1");
+          assert.equal(upkeepNeeded, false);
+        })
+        it("returns false if enough time hasn't passed", async function(){
+          await raffle.enterRaffle({value: raffleEntranceFee});
+          //decreasing time now
+          const scopedInterval = 1; //value in seconds btw
+          //this doesn't work with time travelling back like [interval.toNumber - 1], for some reason
+          //but can be tested by increasing less time than the specified interval, like increasing the time only 1 second
+          await network.provider.send("evm_increaseTime", [Number(scopedInterval)]);
+          //another way to do this
+          await network.provider.request({method: "evm_mine", params: []});
+          const {upkeepNeeded} = await raffle.callStatic.checkUpkeep("0x");
+          assert(!upkeepNeeded);
+        });
+        it("returns true if enough time has passed, has players, ETH, and is open", async()=>{
+          await raffle.enterRaffle({value: raffleEntranceFee});
+          await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
+          await network.provider.request({method: "evm_mine", params: []});
+          const {upkeepNeeded} = await raffle.callStatic.checkUpkeep("0x");
+          assert(upkeepNeeded);
+        })
+      })
     });
